f7881e6a0c4039e1334e5149be2da6c016e28ca1
QuangNhatHung <20182019quangnhathungpanasonic@gmail.com>
2026-03-28 10:53:39 +0700

    create command

--- main.py
+++ main.py
@@ -7,43 +7,43 @@ import os
 
 
 if __name__ == "__main__":
-        choice = int(input(
-                "==================================\n"
-                "[1] Train model v1.0\n"
-                "[2] Train model v2.0\n"
-                "[3] Train model v3.0\n"
-                "[4] Run predict(latest_v3.0)\n"
-                "[5] evaluate model(latest_v3.0)\n"
-                "Choose: "
-                ))
+    choice = int(input(
+        "==================================\n"
+        "[1] Train model v1.0\n"
+        "[2] Train model v2.0\n"
+        "[3] Train model v3.0\n"
+        "[4] Run predict(latest_v3.0)\n"
+        "[5] evaluate model(latest_v3.0)\n"
+        "Choose: "
+        ))
         
-        match(choice):
-            case 1:
-                e = int(input(
-                    "\nEpoch: "
-                    ))
-                os.system('cls' if os.name == 'nt' else 'clear')
-                train_model(epoch=e)
-            case 2:
-                e = int(input(
-                    "\nEpoch: "
-                    ))
-                os.system('cls' if os.name == 'nt' else 'clear')
-                train_model_v2(epoch=e)
-            case 3:
-                e = int(input(
-                    "\nEpoch: "
-                    ))
-                os.system('cls' if os.name == 'nt' else 'clear')
-                train_model_v3(epoch=e)
-            case 4:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                predict()            
-            case 5:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                evaluate_model()
-            case _:
-                print("Invalid choice")
-                os.system('cls' if os.name == 'nt' else 'clear')
+    match(choice):
+        case 1:
+            e = int(input(
+                "\nEpoch: "
+                ))
+            os.system('cls' if os.name == 'nt' else 'clear')
+            train_model(epoch=e)
+        case 2:
+            e = int(input(
+                "\nEpoch: "
+                ))
+            os.system('cls' if os.name == 'nt' else 'clear')
+            train_model_v2(epoch=e)
+        case 3:
+            e = int(input(
+                "\nEpoch: "
+                ))
+            os.system('cls' if os.name == 'nt' else 'clear')
+            train_model_v3(epoch=e)
+        case 4:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            predict()            
+        case 5:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            evaluate_model()
+        case _:
+            print("Invalid choice")
+            os.system('cls' if os.name == 'nt' else 'clear')
 
                    
